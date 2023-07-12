36ecf687e4375e67e71f98bcb83317f8fba5766f
Sikandar-chaudhry <sikandarmukhtar5@gmail.com>
2023-07-12 22:29:39 +0500

    Add flight method : show flight data or 404 error

--- flights/views.py
+++ flights/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from .models import Flight
+from django.shortcuts import get_object_or_404
 # Create your views here.
 
 
 def index (request):
     return render (request , "flights/index.html",{
         "flights" : Flight.objects.all()
+    })
+    
+    
+def flight(request , flight_id):
+    
+    flight = get_object_or_404(Flight, id=flight_id)
+    return render(request, "flights/flight.html", {
+        "flight" : flight
     })
