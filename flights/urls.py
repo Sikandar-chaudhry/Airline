fb1b3c46e18262b0db597e68ced459739e81d266
Sikandar-chaudhry <sikandarmukhtar5@gmail.com>
2023-07-12 22:28:43 +0500

    Add url to see flight data

--- flights/urls.py
+++ flights/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 
 from . import views
 
-#app_name = "flight"
+app_name = "flight"
 
 urlpatterns =[
-    path("",views.index , name="index")
+    path("",views.index , name="index"),
+    path("<int:flight_id>" , views.flight , name="flight")
 ]
