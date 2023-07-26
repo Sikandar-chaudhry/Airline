363de9abcacffc0e04cc7fde10e118ea850fcc57
Sikandar-chaudhry <sikandarmukhtar5@gmail.com>
2023-07-26 14:15:38 +0500

    Use wrong valid flight function

--- flights/tests.py
+++ flights/tests.py
@@ -56,14 +56,14 @@ class FlightTestCase(TestCase):
         f = Flight.objects.get(origin=a1, destination=a1)
 
         c = Client()
-        response = c.get(f"flights/{f.id}")
+        response = c.get(f"/flights/{f.id}")
         self.assertEqual(response.status_code, 200)
 
     def test_invalid_flight_page(self):
         max_id = Flight.objects.all().aggregate(Max("id"))["id__max"]
 
         c = Client()
-        response = c.get(f"flights/{max_id + 1}")
+        response = c.get(f"/flights/{max_id + 1}")
         self.assertEqual(response.status_code, 404)
         
     def test_flight_page_passengers(self):
@@ -72,7 +72,7 @@ class FlightTestCase(TestCase):
         f.passengers.add(p)
 
         c = Client()
-        response = c.get(f"flights/{f.id}")
+        response = c.get(f"/flights/{f.id}")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.context["passengers"].count(), 1)
 
@@ -81,6 +81,6 @@ class FlightTestCase(TestCase):
         p = Passenger.objects.create(first="Alice", last="Adams")
 
         c = Client()
-        response = c.get(f"flights/{f.id}")
+        response = c.get(f"/flights/{f.id}")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.context["non_passengers"].count(), 1)
